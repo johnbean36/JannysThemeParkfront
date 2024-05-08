515e0f6f09d95c03630eea2793106ac254780fb3
Paul Burgess <paul@burgess.io>
2024-05-08 11:15:42 -0400

    Style About page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,8 +6,8 @@ const Nav = () => {
         <div>
             <NavLink to='/'><div className='link'>HOME</div></NavLink>
             <NavLink to='/buytickets'><div className='link'>BUY TICKETS</div></NavLink>
-            <NavLink to="/ticket"><div className='link'>Purchased Tickets</div></NavLink>
-            <NavLink to='/about'><div className='link'>About Us</div></NavLink>
+            <NavLink to="/ticket"><div className='link'>VIEW TICKETS</div></NavLink>
+            <NavLink to='/about'><div className='link'>ABOUT US</div></NavLink>
         </div>
     )
 }
